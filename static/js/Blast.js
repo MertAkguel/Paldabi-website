c1c09f024010b2c2121a57621f0f5ca161d330c9
MertAkguel <Akgue.l@web.de>
2022-12-28 12:41:31 +0100

    update BLAST

--- static/js/Blast.js
+++ static/js/Blast.js
@@ -9,7 +9,7 @@ document.getElementById('matrix').addEventListener('change', function() {
   
     GetFile .onload=function(){
     matrix = GetFile.result;
-    console.log(matrix);
+    //console.log(matrix);
   }
       
       GetFile.readAsText(this.files[0]);
@@ -35,10 +35,10 @@ function get_score(matrix, valA, valB)
 {
     let valA_index = 0;
     let valB_index = 0;
-    console.log(matrix);
-    console.log(matrix[0].length);
+    // console.log(matrix);
+    // console.log(matrix[0].length);
     for(let i = 0; i < matrix[0].length; ++i)
-    {console.log(i);
+    {
         if(matrix[0][i] == valA)
         {
             valA_index = i;
@@ -48,10 +48,90 @@ function get_score(matrix, valA, valB)
             valB_index = i;
         }
     }
-    console.log(valA_index, valB_index);
+    
     return parseInt(matrix[valA_index + 1][valB_index]);
+}
+
+function generateNeighborhood(query, matrix, word_size, score_threshold, threads=1)
+{
+    if (word_size > query.length) 
+    {
+        let output = [];
+        alert("word_size > query.length");
+        return output;
+
+    } 
+    else if (threads <= 0) 
+    {
+        alert("Threads can´t be smaller 1");
+        return;
+    } 
+    else if (query.length < 1) 
+    {
+        alert("Query is to short");
+        return;
+    }
+    let neighborhood = {"infix": new Array(query.length - word_size + 1),
+                        "neighborhood": new Array(query.length - word_size + 1)};
+    
+    const alphabet = "ACDEFGHIKLMNPQRSTVWY";
+    let max = 0;
 
 
+    for (let i = 0; i < alphabet.length; ++i) 
+    {
+        for (let j = alphabet.length - 1; j >= i; --j) 
+        {
+            if (get_score(matrix, alphabet[i], alphabet[j]) > max) 
+            {
+                max = get_score(matrix, alphabet[i], alphabet[j]);
+            }
+        }
+    }
+
+    // Wenn score-threashold zu groß
+    if (score_threshold > (max * word_size)) 
+    {
+        for (let i = 0; i < (query.length - word_size + 1); ++i) 
+        {
+            neighborhood["infix"][i] = query.substr(i, word_size);
+        }
+        return neighborhood;
+    }
+
+    for (let i = 0; i < (query.length - word_size + 1); ++i) 
+    {
+        neighborhood["infix"][i] = query.substr(i, word_size);
+        let temppair = ["", 0];
+        
+
+        //Erstellt die 20^w Permutationen
+        for (let j = 0; j < Math.pow(20, word_size); ++j) 
+        {
+            let help = 0;
+
+            //Bildet den Score von der Permutation und dem Infix
+            for (let n = Math.pow(20, (word_size - 1)); n >= 1; n /= 20) 
+            {
+                temppair[1] += get_score(matrix, neighborhood["infix"][i][help], alphabet[(parseInt(j / n) % 20)]);
+                temppair[0] += alphabet[(parseInt(j / n) % 20)];
+               
+                ++help;
+            }
+            
+            //Prüft ob Treshold größer als errechneter Score ist
+            if (temppair[1] >= score_threshold) 
+            {
+                neighborhood["neighborhood"][i] = temppair;
+                
+            }
+            temppair[0] = "";
+            temppair[1] = 0;
+
+        }
+    }
+
+    return neighborhood;
 }
 
 
@@ -76,5 +156,13 @@ function get_score(matrix, valA, valB)
 function generateNeighborhoods()
 {
     matrix = load_matrix(matrix);
+    const query = document.getElementById('sequence').value;
+    const word_size = parseInt(document.getElementById('word_size').value);
+    const score_threshold = parseInt(document.getElementById('threshold').value);
+    const threads = parseInt(document.getElementById('thread').value);
+
+    neighborhoods = generateNeighborhood(query, matrix, word_size, score_threshold, threads);
+    console.log(neighborhoods);
+
     
 }
